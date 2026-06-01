023a228c14a0dc2a983401c7110cca05193afdfa
visalsing <sovisalsing288@gmail.com>
2026-06-01 15:23:53 +0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio-vuejs/',
+  // base: '/my-portfolio-vuejs/',
   // base: '/my-portfolio-sing/',
+  base: '/',
   plugins: [vue(), tailwindcss()],
 })
